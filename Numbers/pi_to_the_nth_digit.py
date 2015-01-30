ffb65e15b816d45cb19c3ad40adc4aebb271d2d9
Florian Richter <florian.richter@student.hpi.uni-potsdam.de>
2015-01-30 21:24:04 +0100

    fix pi calculation to support arbitrary precision

--- Numbers/pi_to_the_nth_digit.py
+++ Numbers/pi_to_the_nth_digit.py
@@ -1,14 +1,23 @@
 # **Find PI to the Nth Digit** - Enter a number and have the program generate PI up to that many decimal places. Keep a limit to how far the program will go.
 
-import math
-import decimal
+from decimal import Decimal, getcontext, ROUND_DOWN
+from math import trunc
 
-getcontext().prec = 50
+precision = int(input("Calculate pi to the nth digit: "))
+getcontext().prec = precision + 2
+getcontext().rounding = ROUND_DOWN
 
-n = input("Calculate Pi to this decimal place: ")
-if int(n) < 50:
-	result = Decimal(math.trunc(math.pi * pow(10, int(n)))/pow(10, int(n)))
-	print(result)
-else:
-	print(Decimal(math.pi))
-	print("The maximum precision is 50.")
+pi = Decimal(0)
+base16 = Decimal(1)
+
+for x in range(0, precision):
+    a = Decimal(4) / Decimal(8 * x + 1)
+    b = Decimal(-2) / Decimal(8.0 * x + 4)
+    c = Decimal(-1) / Decimal(8.0 * x + 5)
+    d = Decimal(-1) / Decimal(8.0 * x + 6)
+    pi += Decimal(a + b + c + d) / Decimal(base16)
+    base16 *= Decimal(16)
+
+getcontext().prec = precision
+
+print(+pi)
