de5dee6b3775aa51fe1e88536619cf2e7ab11299
Florian Richter <florian.richter@student.hpi.uni-potsdam.de>
2015-01-30 22:14:31 +0100

    fix e calculation to support arbitrary precision

--- Numbers/e_to_the_nth_digit.py
+++ Numbers/e_to_the_nth_digit.py
@@ -1,14 +1,17 @@
 # **Find e to the Nth Digit** - Just like the previous problem, but with e instead of PI. Enter a number and have the program generate e up to that many decimal places. Keep a limit to how far the program will go.
 
-import math
-import decimal
+from decimal import Decimal, getcontext, ROUND_DOWN
+from math import factorial
 
-getcontext().prec = 50
+precision = int(input("Calculate e to the nth digit: "))
+getcontext().prec = precision + 5
+getcontext().rounding = ROUND_DOWN
 
-n = input("Calculate 'e' to this decimal place: ")
-if int(n) < 50:
-	result = Decimal(math.trunc(math.e * pow(10, int(n)))/pow(10, int(n)))
-	print(result)
-else:
-	print(Decimal(math.e))
-	print("The maximum precision is 50.")
+e = Decimal(0)
+
+for x in range(0, precision + 1):
+    e += Decimal(2 * x + 1) / Decimal(factorial(2 * x))
+
+getcontext().prec = precision
+
+print(+e)
