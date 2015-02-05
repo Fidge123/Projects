fa21b07dd3d16e8cd6994940779db2c9c6001fb6
Florian Richter <florian.richter@student.hpi.uni-potsdam.de>
2015-02-05 17:23:04 +0100

    add basic GUI for next_prime.py using tkinter

--- Numbers/next_prime.py
+++ Numbers/next_prime.py
@@ -1,30 +1,51 @@
 # **Next Prime Number** - Have the program find prime numbers until the user chooses to stop asking for the next one.
 
-calc_next = "y"
-primes = []
-current_number = 2
-is_prime = True
-found_prime = False
+import tkinter
 
-while calc_next == "y":
-    calc_next = input("Calculate next prime (y/n)? ")
+class Application(tkinter.Frame):
+    def __init__(self, parent=None):
+        tkinter.Frame.__init__(self,parent)
+        self.parent = parent
 
-    while calc_next != "y" and calc_next != "n":
-        calc_next = input("Invalid input! Calculate next prime (y/n)? ")
+        self.primes = [2]
+        self.current_number = 2
+        self.is_prime = True
+        self.found_prime = False
 
-    while found_prime == False:
-        for p in primes:
-            if current_number % p == 0:
-                is_prime = False
+        self.initialize()
 
-        if is_prime == True:
-            primes.append(current_number)
-            found_prime = True
-        else:
-            current_number += 1
-            is_prime = True
+    def initialize(self):
+        self.grid()
 
-    print("Next prime is ", current_number)
+        self.prime_label = tkinter.Label(self)
+        self.prime_label["text"] = self.current_number
+        self.prime_label.grid(column=0, row=0)
 
-    found_prime = False
-    current_number += 1
+        self.next_prime_button = tkinter.Button(self, text="Next", command=self.next_prime)
+        self.next_prime_button.grid(column=0, row=1)
+
+        self.quit_button = tkinter.Button(self, text="QUIT!", command=root.destroy)
+        self.quit_button.grid(column=1, row=1)
+
+    def next_prime(self):
+        while self.found_prime == False:
+            for p in self.primes:
+                if self.current_number % p == 0:
+                    self.is_prime = False
+
+            if self.is_prime == True:
+                self.primes.append(self.current_number)
+                self.found_prime = True
+            else:
+                self.current_number += 1
+                self.is_prime = True
+
+        self.prime_label["text"] = self.current_number
+
+        self.found_prime = False
+        self.current_number += 1
+
+if __name__ == "__main__":
+    root = tkinter.Tk()
+    app = Application(parent=root)
+    app.mainloop()
